8bb467ab9d74e72365d4c25d49885374b5b932d0
Mario Irigoyen <89858719+Tar-Mairon24@users.noreply.github.com>
2025-10-16 14:02:15 -0600

    Update Git credentials ID in cd.groovy

--- jenkins-shared-library/backend/vars/cd.groovy
+++ jenkins-shared-library/backend/vars/cd.groovy
@@ -53,7 +53,7 @@ def call(Map params) {
                 branches: [[name: 'main']],
                 userRemoteConfigs: [[
                     url: 'https://github.com/Tar-Mairon24/inmo-pipeline-properties.git',
-                    credentialsId: 'Github_Token'
+                    credentialsId: 'github-credentials'
                 ]],
                 extensions: [
                     [$class: 'RelativeTargetDirectory', relativeTargetDir: 'properties-repo']
@@ -121,7 +121,7 @@ def call(Map params) {
                 branches: [[name: 'main']],
                 userRemoteConfigs: [[
                     url: 'https://github.com/Tar-Mairon24/inmo-pipeline-config.git',
-                    credentialsId: 'Github_Token'
+                    credentialsId: 'github-credentials'
                 ]],
                 extensions: [
                     [$class: 'RelativeTargetDirectory', relativeTargetDir: 'config-repo']
